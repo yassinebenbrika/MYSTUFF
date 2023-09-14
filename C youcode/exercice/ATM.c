dde4a93b5e0d40cf62cf4feba4059c20e7fde28f
yassinebenbrika <yassinerodri@gmail.com>
2023-09-14 11:18:27 +0100

    new file

---
+++ C youcode/exercice/ATM.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdbool.h> // Include this header for boolean data type
+int main()
+{
+    int choice, balance = 1000, withdraw, deposit;
+    char continueChoice;
+    int password = 1234; // Default password
+    int enteredPassword;
+    printf("Welcome to XYZ Bank ATM\n");
+    printf("Please enter the initial password: ");
+    scanf("%d", &password);
+    do
+    {
+        printf("\nMenu:\n");
+        printf("1. Balance Inquiry\n");
+        printf("2. Withdrawal\n");
+        printf("3. Deposit\n");
+        printf("4. Change Password\n");
+        printf("5. Exit\n");
+        printf("Please enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            // Implement balance inquiry logic
+            printf("Your balance is %d\n", balance);
+            break;
+        case 2:
+            // Implement withdrawal logic
+            printf("How much do you want to withdraw?\n");
+            scanf("%d", &withdraw);
+            if (withdraw <= balance)
+            {
+                balance -= withdraw;
+                printf("Withdrawal successful. Your balance is: %d\n", balance);
+            }
+            else
+            {
+                printf("Insufficient balance.\n");
+            }
+            break;
+        case 3:
+            // Implement deposit logic
+            printf("How much do you want to deposit?\n");
+            scanf("%d", &deposit);
+            balance += deposit;
+            printf("Deposit successful. Updated balance: %d\n", balance);
+            break;
+        case 4:
+            // Implement password change logic
+            printf("Enter the current password: ");
+            scanf("%d", &enteredPassword);
+            if (enteredPassword == password)
+            {
+                printf("Enter the new password: ");
+                scanf("%d", &password);
+                printf("Password changed successfully.\n");
+            }
+            else
+            {
+                printf("Incorrect password. Password change failed.\n");
+            }
+            break;
+        case 5:
+            printf("Thank you for using XYZ Bank ATM. Goodbye!\n");
+            break;
+        default:
+            printf("Invalid choice. Please enter a valid option.\n");
+        }
+        if (choice != 5)
+        {
+            printf("Do you want to continue (y/n)? ");
+            scanf(" %c", &continueChoice);
+        }
+    } while (choice != 5 && (continueChoice == 'y' || continueChoice == 'Y'));
+    return 0;
+}
