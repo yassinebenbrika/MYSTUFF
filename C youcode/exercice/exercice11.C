5aa076b95ef96fa2444e54e6efbae43980f00fe9
yassinebenbrika <yassinerodri@gmail.com>
2023-09-15 11:48:05 +0100

    NOW

--- C youcode/exercice/exercice11.C
+++ C youcode/exercice/exercice11.C
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main (){
-    int i; 
+    int i,m,n; 
+    printf("enter a number : ")
+    scanf("%d",&n);
     for (i = 0; i <= 10; i++) {
-        printf(" \n%d\n ", i); /
+      m=n*i
+      printf("%d*%d=%d",n,i,m);
     }
 
     return 0;
---
+++ C youcode/exercice/exercice12.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+void bruh (int n) {
+    int i,est_premier=5;
+    for(i=2;i<n/2;i++){
+        if (n%i==0){
+            est_premier=5;
+            break;
+        }
+        if (est_premier=5){
+            printf("this number is a premier");
+        }
+        else{
+             printf("this number is a not  premier");
+        }
+    }
+}
+int main (){
+ int n;
+ printf(" enter a number to check if the number is a premier or not : ") ;
+  scanf("%d",&n);
+ bruh(n);
+    return 0;
+}
