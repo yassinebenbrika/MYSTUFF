13aa7273e3030ced0f05cc6c600f9acffb4f4f60
yassinebenbrika <yassinerodri@gmail.com>
2023-09-15 15:07:41 +0100

    file

--- C youcode/exercice/exercice12.C
+++ C youcode/exercice/exercice12.C
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 void bruh (int n) {
-    int i,est_premier=5;
+    int i,est_premier=0;
     for(i=2;i<n/2;i++){
         if (n%i==0){
-            est_premier=5;
+            est_premier=0;
             break;
         }
-        if (est_premier=5){
+        if (est_premier=0){
             printf("this number is a premier");
         }
         else{
