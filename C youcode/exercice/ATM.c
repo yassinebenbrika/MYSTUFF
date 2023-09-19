681817ed8435b7496754065adb033b44e164a59d
yassinebenbrika <yassinerodri@gmail.com>
2023-09-19 11:59:40 +0100

    FILE

--- C youcode/exercice/ATM.c
+++ C youcode/exercice/ATM.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+
 char continueChoice;
 int choose;
 int n;
@@ -32,57 +33,49 @@ int linearSearch() {
 }
 
 int main() {
-   
-    do
-    {   
-    printf("How many numbers do you want to sort: ");
-    scanf("%d", &n);
+    do {
+        printf("How many numbers do you want to sort: ");
+        scanf("%d", &n);
 
-    for (int i = 0; i < n; i++) {
-        printf("Enter a number: ");
-        scanf("%d", &N[i]);
+        for (int i = 0; i < n; i++) {
+            printf("Enter a number: ");
+            scanf("%d", &N[i]);
+        }
 
-    }
-    printf("1-sorted arrays\n");
-    printf("2-searche about index\n");
-    printf("enter your choose :");
-    scanf("%d",&choose);
+        printf("1 - Sort the array\n");
+        printf("2 - Search for an element's index\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choose);
 
-switch (choose)
-{
-case 1:
- bubbleSort();
+        switch (choose) {
+            case 1:
+                bubbleSort();
+                printf("Sorted array: ");
+                for (int i = 0; i < n; i++) {
+                    printf("%d ", N[i]);
+                }
+                printf("\n");
+                break;
+            case 2:
+                printf("Enter a number to search for: ");
+                scanf("%d", &target);
 
-    printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", N[i]);
-    }
-    printf("\n");
-    break;
-case 2:
-       printf("Enter a number to search for: ");
-    scanf("%d", &target);
+                int index = linearSearch();
 
-    int index = linearSearch();
+                if (index != -1) {
+                    printf("Element %d found at index %d\n", target, index);
+                } else {
+                    printf("Element %d not found in the array\n", target);
+                }
+                break;
+            default:
+                printf("Invalid choice. Please enter a valid option.\n");
+                break;
+        }
 
-    if (index != -1) {
-        printf("Element %d found at index %d\n", target, index);
-    } else {
-        printf("Element %d not found in the array\n", target);
-    }
-    
-default:
-        printf("Invalid choice. Please enter a valid option.\n");
-    break;
-}
-    
         printf("Do you want to continue (y/n)? ");
-            scanf(" %c", &continueChoice);
-   
-             } while (continueChoice=='y'|| continueChoice=='Y');
-    return 0;
-        
+        scanf(" %c", &continueChoice);
+    } while (continueChoice == 'y' || continueChoice == 'Y');
 
-   
-    
+    return 0;
 }
